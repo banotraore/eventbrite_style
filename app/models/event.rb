1ffffe3e5877809c746a020e03850162778ead43
banotraore <banotraore@gmail.com>
2019-05-06 21:23:52 +0200

    update user mailers

---
+++ app/models/event.rb
@@ -0,0 +1,11 @@
+class Event < ApplicationRecord
+  has_many :attendances
+  belongs_to :user
+  validates :start_date, inclusion: { in: (Date.today..Date.today+5.years) }, presence: true
+  validates :duration, numericality:  { greater_than: 5 }, presence: true
+  validates :title, length: { in: 5..140 }, presence: true
+  validates :description, length: { in: 20..1000 }, presence: true
+  validates :price, numericality: { greater_than: 0, less_than: 1001 }, presence: true
+  validates :location, presence: true
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Purge the user table before create 10 users to fill it
+User.destroy_all
+num = 1
+5.times do
+  fake_password = Faker::Lorem.characters(10)
+  User.create(
+    email:"test_thp_bt#{num}@yopmail.com",
+    encrypted_password: fake_password,
+    description: Faker::TvShows::GameOfThrones.quote,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+  num += 1
+
+end
