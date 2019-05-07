88c9ec031feaaa84db9ebe6978ddd2aac365767b
banotraore <banotraore@gmail.com>
2019-05-08 00:22:35 +0200

    update num??

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'events#index'
-  resources :events_index
+  #resources :events_index
   get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :events
+ resources :users, only: [:show, :index, :edit, :update]
+
 
 end
